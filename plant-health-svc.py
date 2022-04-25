42d3fcb54493fc3a12f77be1aa25c7722558fe3f
mslipenchuk267 <mslipenchuk267@gmail.com>
2022-04-24 23:54:23 -0400

    Connecting to local mqtt broker and subbing to moisture topic

--- plant-health-svc.py
+++ plant-health-svc.py
@@ -1,24 +1,43 @@
-import time    
+import time
+import paho.mqtt.client as paho
 import mysql.connector
 
 import config
 
+broker=config.mqtt_host
 
-mydb = mysql.connector.connect(
-  host=config.host,
-  user=config.user,
-  password=config.password
-)
+#define callback
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("moisture")
 
-print(mydb)
+def on_message(client, userdata, message):
+    #time.sleep(1)
+    print("received message =", str(message.payload.decode("utf-8")))
 
-mycursor = mydb.cursor()
+client= paho.Client("client-001")
+client.connect(broker)
+client.on_connect = on_connect
+client.on_message=on_message
 
-sql = "INSERT INTO `plant-health-db`.moisture (sensor_name, obs_time, val) VALUES (%s, %s, %s)"
-val = ("sensor_1", time.strftime('%Y-%m-%d %H:%M:%S'), 99)
+client.loop_forever()
 
-mycursor.execute(sql, val)
 
-mydb.commit()
-
-print(mycursor.rowcount, "record inserted.")
+#mydb = mysql.connector.connect(
+#  host=config.mysql_host,
+#  user=config.mysql_user,
+#  password=config.mysql_password
+#)
+#
+#print(mydb)
+#
+#mycursor = mydb.cursor()
+#
+#sql = "INSERT INTO `plant-health-db`.moisture (sensor_name, obs_time, val) VALUES (%s, %s, %s)"
+#val = ("sensor_1", time.strftime('%Y-%m-%d %H:%M:%S'), 99)
+#
+#mycursor.execute(sql, val)
+#
+#mydb.commit()
+#
+#print(mycursor.rowcount, "record inserted.")
